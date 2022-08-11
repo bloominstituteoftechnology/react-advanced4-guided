4fe150a969d087983e02b0528a77d1abf600e0b0
Ladrillo <gabriel@cabrejas.net>
2022-08-11 17:25:13 +0200

    prep

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -4,70 +4,41 @@ import '@testing-library/jest-dom/extend-expect'
 import App from './TestingReact'
 
 beforeEach(() => {
-  render(<App />)
+
 })
 
 describe('<App />', () => {
   test('renders without errors', () => {
-    // screen.debug()
+
   })
-  test('Todos: text renders', () => {
-    const element = screen.queryByText('Todos:')
-    expect(element).toBeInTheDocument()
-    expect(element).toBeVisible()
+  test('Todos: text renders (capture element using queryByText)', () => {
+
   })
-  test('Todos: text renders', () => {
-    screen.getByText('todos:', { exact: false })
+  test('Todos: text renders (capture element using getByText)', () => {
+
   })
-  test('Todos: text renders', () => {
-    const h2 = document.querySelector('h2')
-    expect(h2.textContent).toMatch(/todos/i)
-    expect(h2).toBeInTheDocument()
-    expect(h2).toBeVisible()
+  test('Todos: text renders (capture element using querySelector)', () => {
+
   })
-  test('Todos: text renders', () => {
-    screen.getByTestId('todoListHeading')
+  test('Todos: text renders (capture element using data attribute)', () => {
+
   })
-  test('Submit button disabled while input empty', () => {
-    const submitButton = screen.getByText('Submit Todo')
-    expect(submitButton).toBeDisabled()
+  test('Submit button is disabled while input empty', () => {
+
   })
   test('Typing a character enables the input', () => {
-    const submitButton = screen.getByText('Submit Todo')
-    const input = screen.getByPlaceholderText('Type todo')
-    fireEvent.change(input, { target: { value: 'a' } })
-    expect(submitButton).toBeEnabled()
+
   })
   test('Can complete a todo', () => {
-    let walkDog = screen.getByText('Walk the dog')
-    fireEvent.click(walkDog)
-    expect(walkDog.textContent).toBe('Walk the dog ✔️')
+
   })
   test('Can submit new todo', () => {
-    const input = screen.getByPlaceholderText('Type todo')
-    const submitButton = screen.getByText('Submit Todo')
-    fireEvent.change(input, { target: { value: 'Use Postman' } })
-    fireEvent.click(submitButton)
-    screen.getByText('Use Postman')
+
   })
   test('Submitting clears the input', () => {
-    const input = screen.getByPlaceholderText('Type todo')
-    const submitButton = screen.getByText('Submit Todo')
-    fireEvent.change(input, { target: { value: 'Use Postman' } })
-    expect(input).toHaveValue('Use Postman')
-    fireEvent.click(submitButton)
-    expect(input).toHaveValue('')
+
   })
   test('Can hide completed', () => {
-    const hideShowButton = screen.getByText('Hide Completed')
-    screen.getByText('Walk the dog')
-    screen.getByText('Learn React', { exact: false })
-    screen.getByText('Have fun')
-    fireEvent.click(hideShowButton)
-    screen.getByText('Show Completed')
-    screen.getByText('Walk the dog')
-    screen.getByText('Have fun')
-    const gone = screen.queryByText('Learn React', { exact: false })
-    expect(gone).not.toBeInTheDocument()
+
   })
 })
