55b9c53434287171109fb6d27e5f838cfaa8607f
Ladrillo <gabriel@cabrejas.net>
2022-08-11 19:16:06 +0200

    committing to "lecture"

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -43,7 +43,7 @@ describe('<App />', () => {
     const input = screen.getByPlaceholderText('Type todo')
     const button = screen.getByText('Submit Todo')
     fireEvent.change(input, { target: { value: 'A' } })
-    screen.debug()
+    expect(input).toHaveValue('A')
   })
   test('Can complete a todo', () => {
 
