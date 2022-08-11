f3b96db7c28d05a2025190f76956a95966149501
Ladrillo <gabriel@cabrejas.net>
2022-08-11 18:26:26 +0200

    committing to "lecture"

--- frontend/components/TestingReact.test.js
+++ frontend/components/TestingReact.test.js
@@ -16,7 +16,7 @@ describe('<App />', () => {
     // screen.debug()
   })
   test.only('Todos: text renders (capture element using queryByText)', () => {
-    const todosHeading
+    const todosHeading = screen.getByText('Todzkjvhlkjdhfdskjlbhjkhdos:')
   })
   test('Todos: text renders (capture element using getByText)', () => {
 
